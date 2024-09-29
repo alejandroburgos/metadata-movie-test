aac5c4b083e62f418494961ecc96290e6c3c5628
Alejandro Burgos <aburgosm@eservicios.indra.es>
2024-09-29 22:29:53 +0200

    added spinner, short titles and placeholder

--- src/components/ListMovie2D.js
+++ src/components/ListMovie2D.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchMovieById, toggleFavorite } from "../redux/moviesSlice";
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
+import { ProgressSpinner } from "primereact/progressspinner";
+import { Tooltip } from "primereact/tooltip";
 import { useNavigate } from "react-router-dom";
 
 export const ListMovie2D = ({ movies, favorites, isFavoriteView }) => {
@@ -24,16 +26,39 @@ export const ListMovie2D = ({ movies, favorites, isFavoriteView }) => {
   }, [dispatch, favorites, isFavoriteView]);
 
   const displayedMovies = isFavoriteView ? arrFavorites : movies;
+  const placeholder = "https://via.placeholder.com/300x450.png?text=No+Image";
+  const shortTitle = (title) => {
+    if (title.length > 20) {
+      return title.slice(0, 20) + "...";
+    }
+    return title;
+  };
 
   return (
     <div className="list-movie-2d">
-      {loading && <p>Cargando...</p>}
+      {loading && (
+        <ProgressSpinner
+          style={{ width: "50px", height: "50px" }}
+          strokeWidth="8"
+          animationDuration=".5s"
+        />
+      )}
       <div className="list-movie-2d__grid">
         {displayedMovies && displayedMovies.length > 0 ? (
           displayedMovies.map((movie) => (
             <div key={movie.imdbID} className="list-movie-2d__column">
               <Card
-                title={movie.Title}
+                title={
+                  <>
+                    <span id={`title-${movie.imdbID}`}>
+                      {shortTitle(movie.Title)}
+                    </span>
+                    <Tooltip
+                      target={`#title-${movie.imdbID}`}
+                      content={movie.Title}
+                    />
+                  </>
+                }
                 className="list-movie-2d__card"
                 footer={
                   <div className="list-movie-2d__footer">
@@ -55,7 +80,11 @@ export const ListMovie2D = ({ movies, favorites, isFavoriteView }) => {
                 }
               >
                 <img
-                  src={movie.Poster}
+                  src={
+                    movie.Poster !== "N/A" || !movie.Poster
+                      ? movie.Poster
+                      : placeholder
+                  }
                   alt={movie.Title}
                   className="list-movie-2d__image"
                 />
@@ -63,7 +92,9 @@ export const ListMovie2D = ({ movies, favorites, isFavoriteView }) => {
             </div>
           ))
         ) : (
-          <p>No hay películas para mostrar.</p>
+          <p className="list-movie-2d__grid__empty">
+            No hay películas disponibles
+          </p>
         )}
       </div>
     </div>
